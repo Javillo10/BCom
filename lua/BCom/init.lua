566e926c86ff5d77fbe7363426f650580c0235bf
Javillo10 <fjaviergrodriguez@gmail.com>
2025-10-21 11:58:35 +0200

    Fix lua/Bcom/init.lua

--- lua/BCom/init.lua
+++ lua/BCom/init.lua
@@ -5,18 +5,18 @@ M.config = {
 }
 
 function M.setup(opts)
-    M.config = vim.tbl_deep_extend("forece", M.config, opts or {})
+    M.config = vim.tbl_deep_extend("force", M.config, opts or {})
+end
 
-        local funciones = require("BCom.funciones")
-        local ventanaAprender = require("BCom.ventanaAprender")
-        local leerCSV = require("BCom.leerCSV")
-        local abrirVentanaInicio = require("BCom.abrirVentanaInicio")
+    local funciones = require("BCom.funciones")
+    local ventanaAprender = require("BCom.ventanaAprender")
+    local leerCSV = require("BCom.leerCSV")
+    local abrirVentanaInicio = require("BCom.abrirVentanaInicio")
 
-        --funciones.config = M.config
-        --ventanaAprender.config = M.config
-        --leerCSV.config = M.config
-        abrirVentanaInicio.config = M.config
+    --funciones.config = M.config
+    --ventanaAprender.config = M.config
+    --leerCSV.config = M.config
+    --abrirVentanaInicio.config = M.config
 
-    end
         
 return M
