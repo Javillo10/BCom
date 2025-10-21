93464d43acbe5f8b202000f5596be0bb93c9018a
Javillo10 <fjaviergrodriguez@gmail.com>
2025-10-21 11:49:36 +0200

    Added initial panel configuration

--- lua/BCom/init.lua
+++ lua/BCom/init.lua
@@ -1,8 +1,22 @@
 local M = {}
 
-M.funciones = require("BCom.funciones")
-M.ventanaAprender = require("BCom.ventanaAprender")
-M.leerCSV = require("BCom.leerCSV")
-M.abrirVentanaInicio = require("BCom.abrirVentanaInicio")
+M.config = {
+    showInitPanel = true
+}
 
+function M.setup(opts)
+    M.config = vim.tbl_deep_extend("forece", M.config, opts or {})
+
+        local funciones = require("BCom.funciones")
+        local ventanaAprender = require("BCom.ventanaAprender")
+        local leerCSV = require("BCom.leerCSV")
+        local abrirVentanaInicio = require("BCom.abrirVentanaInicio")
+
+        --funciones.config = M.config
+        --ventanaAprender.config = M.config
+        --leerCSV.config = M.config
+        abrirVentanaInicio.config = M.config
+
+    end
+        
 return M
--- plugin/bcom.lua
+++ plugin/bcom.lua
@@ -1,20 +1,23 @@
-local funciones = require("BCom.funciones")
-local csv = require("BCom.leerCSV")
-local ventanaInicio = require("BCom.abrirVentanaInicio")
-local ventanaAprender = require("BCom.ventanaAprender")
+local bcom = require("BCom")
+--local funciones = require("BCom.funciones")
+--local csv = require("BCom.leerCSV")
+--local ventanaInicio = require("BCom.abrirVentanaInicio")
+--local ventanaAprender = require("BCom.ventanaAprender")
 
 math.randomseed(os.time())
 
-csv.leerCSV()
+bcom.csv.leerCSV()
 
-ventanaInicio.abrirVentanaInicio()
+if bcom.config.showInitPanel then
+    bcom.ventanaInicio.abrirVentanaInicio()
+end
 
 vim.api.nvim_create_user_command("Bcom", function(opts)
-  funciones.busqueda(opts.args)
+  bcom.funciones.busqueda(opts.args)
 end, { nargs = 1 })
 
 vim.api.nvim_create_user_command("BcomA", function()
-  ventanaAprender.ventanaAprender()
+  bcom.ventanaAprender.ventanaAprender()
 end, {})
 
 
