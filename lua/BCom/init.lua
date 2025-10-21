bfeefb340ca1204b7362ecb154cfee828a69fc86
Javillo10 <fjaviergrodriguez@gmail.com>
2025-10-21 12:24:43 +0200

    Fix lua/Bcom/init.lua and plugin/bcom.lua

--- lua/BCom/init.lua
+++ lua/BCom/init.lua
@@ -1,22 +1,29 @@
 local M = {}
 
-M.config = {
-    showInitPanel = true
-}
-
-function M.setup(opts)
-    M.config = vim.tbl_deep_extend("force", M.config, opts or {})
-end
-
     local funciones = require("BCom.funciones")
     local ventanaAprender = require("BCom.ventanaAprender")
     local leerCSV = require("BCom.leerCSV")
     local abrirVentanaInicio = require("BCom.abrirVentanaInicio")
 
+M.setup = function(opts)
+    opts = opts or {}
+
+    local showInitPanel = opts.showInitPanel
+    if showInitPanel == nil then
+        showInitPanel = true
+    end
+    
+
+    leerCSV.leerCSV();
+
+    if showInitPanel then
+        abrirVentanaInicio.abrirVentanaInicio()
+    end
+
     --funciones.config = M.config
     --ventanaAprender.config = M.config
     --leerCSV.config = M.config
     --abrirVentanaInicio.config = M.config
+end
 
         
-return M
